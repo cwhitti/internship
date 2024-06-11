913cde95352c6285ac3ecf39e6f2789a4f7b1f35
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-06-11 10:34:09 -0400

    updated files

--- Server/configure_docker.sh
+++ Server/configure_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 docker build -t chat_server_image .
 
-docker run --name chat_server_container -p 8080:8080 chat_server_image
+docker run --name chat_server_container -p 1110:1110 chat_server_image
