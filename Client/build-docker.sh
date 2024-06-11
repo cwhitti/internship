e479486aae5f584e056e90550d9cdf72c7035581
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-06-11 10:58:27 -0400

    restructured docker file

--- Client/build-docker.sh
+++ Client/build-docker.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-docker build -t chat_client_image_claire .
+RUN=false
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <username> <run>"
+    exit 1
+fi
+
+
+USERNAME=$1
+
+PROPERTIES_FILE="src/properties/remote/${USERNAME}.properties"
+
+# VULNERABILITY: CHECK IF FILE EXISTS
+if [ ! -f "$PROPERTIES_FILE" ]; then
+    echo "Error: Property file '$PROPERTIES_FILE' does not exist."
+    exit 1
+fi
+
+echo "building image: chat_client_image_$USERNAME" 
+
+docker build --build-arg USERNAME=$USERNAME -t chat_client_image_$USERNAME .
+
+if [ "$2" = true ]; then
+    echo "Running client..."
+    docker run --name chat_client_container_$USERNAME -p 5001:5001 chat_client_image_$USERNAME
+fi
--- Client/run-docker-claire.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name chat_client_container_claire -p 5001:5001 chat_client_image_claire
