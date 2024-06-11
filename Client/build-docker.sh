b060c1a568f93519b93a6b532094448d19b43e55
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-06-11 13:24:30 -0400

    Hopefully fixed the port bug

--- Client/build-docker.sh
+++ Client/build-docker.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# set variables
 RUN=false
+DIR=remote
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <username> <run>"
+    echo "Usage: $0 <username> <bool:run>"
     exit 1
 fi
 
-
 USERNAME=$1
 
-PROPERTIES_FILE="src/properties/remote/${USERNAME}.properties"
+PROPERTIES_FILE="src/properties/${DIR}/${USERNAME}.properties"
 
 # VULNERABILITY: CHECK IF FILE EXISTS
 if [ ! -f "$PROPERTIES_FILE" ]; then
@@ -18,11 +19,9 @@ if [ ! -f "$PROPERTIES_FILE" ]; then
     exit 1
 fi
 
-echo "building image: chat_client_image_$USERNAME" 
-
-docker build --build-arg USERNAME=$USERNAME -t chat_client_image_$USERNAME .
+export PORT=$(grep '^MY_PORT' "$PROPERTIES_FILE" | cut -d '=' -f 2 | tr -d '\r' | tr -d '\n' | tr -d ' ')
 
 if [ "$2" = true ]; then
     echo "Running client..."
-    docker run --name chat_client_container_$USERNAME -p 5001:5001 chat_client_image_$USERNAME
+    docker run --name client_container_$USERNAME -p $PORT:$PORT client_image_$USERNAME 
 fi
