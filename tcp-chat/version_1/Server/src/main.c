5a1f360ec5a0e850447434866927345f98991970
cwhitti <112997645+cwhitti@users.noreply.github.com>
2024-06-13 09:08:45 -0400

    6/13/24

--- tcp-chat/version_1/Server/src/main.c
+++ tcp-chat/version_1/Server/src/main.c
@@ -6,7 +6,6 @@
 
 int main(int argc, char** argv)
 {
-     printf("HIELLOxxxx");
     int server_socket;                 // descriptor of server socket
     struct sockaddr_in server_address; // for naming the server's listening socket
     ThreadArgs threadArgs; // create thread args
@@ -14,8 +13,6 @@ int main(int argc, char** argv)
     int yes = 1;
     int port;
     Properties* properties;
-
-    printf("HIELLO");
 
     properties = property_read_properties(argv[1]);
     sscanf(property_get_property(properties, "SERVER_PORT"), "%d", &port);
